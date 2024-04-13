ac7a272353d70906b8e911f68bb0659bbc1dd2f3
Your name <example@example.com>
2024-04-13 17:56:20 +0300

    add js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/scss/bootstrap.scss";
 
-let lightStatus = false;
+const redLightToggler = document.getElementById("red-light-toggler");
+const greenLightToggler = document.getElementById("green-light-toggler");
 
-const lightToggler = document.getElementById("light-toggler");
-lightToggler.addEventListener('click', () => {
-    
-})
+function toggle (led) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://192.168.4.1/?led=" + led);
+    xhr.send();
+}
+
+redLightToggler.addEventListener('click', () => toggle(1));
+greenLightToggler.addEventListener('click', () => toggle(2));
